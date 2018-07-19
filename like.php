f0c940e7b11980f9226cbebb24b13aa9668ecfc3
antonpurwadi <antonpurwadi03@gmail.com>
2018-07-19 14:03:11 +0700

    Update like.php

--- like.php
+++ like.php
@@ -1,36 +1,38 @@
 <?php
+
 ####Copyright By Janu fb.com/lastducky####
 ####Thanks For Indra Swastika(fungsi.php)####
 ####Change This Copyright Doesn't Make You a Coder :) ####
-#######EDIT THIS AREA#########
+
+
+
+echo "Username?\nInput : ";
+$username =  trim(fgets(STDIN));
 #######END OF EDIT AREA########
 require_once('func.php');
-echo "Mohon Input\nUsername : ";
-$username =  trim(fgets(STDIN));
-if (!file_exists("$username.ig")) {
-echo "Mohon Input\nPassword : ";
-$password = trim(fgets(STDIN));
+if (!file_exists($username.".ig")) {
+    echo "Password?\nInput : ";
+    $password = trim(fgets(STDIN));
     $log = masuk($username, $password);
     if ($log == "data berhasil diinput") {
-        echo "Berhasil Input Data, silahkan Ketik ulang php like.php\n";
+        echo "Berhasil Input Data\n";
     } else {
-        echo "Gagal Input Data";
+        echo "Gagal Input Data\n";
     }
 } else {
+    echo "Type? (1 = Gak Follback)\nInput : ";
+    $type = trim(fgets(STDIN));
+    echo "Jeda Setiap Sudah Unfoll? (max = 50 jika lebih, akan otomatis di-Set 50)\nInput : ";
+    $jeda = trim(fgets(STDIN));
+    if($jeda>50) $jeda = 50;
+    echo "Jeda sudah di atur sebanyak setiap $jeda orang yang di Unfollow\n";
+    if($type==1) $type = "true";
     $gip    = file_get_contents($username.'.ig');
     $gip    = json_decode($gip);
-    echo "Hai, $gip->username [".$gip->id."] Klik Enter Untuk Melanjutkan..";
-    $skip = trim(fgets(STDIN));
-    echo "Masukan Waktu Jeda?\nSatuan Detik : ";
-    $jeda = trim(fgets(STDIN));
-    if($jeda<60){
-        echo "Waktu telah diatur 60 detik, karena waktu yang ada masukan rentan terhadap keamanan akun.\n";
-        $jeda = 60;
-    }
     $cekuki = instagram(1, $gip->useragent, 'feed/timeline/', $gip->cookies);
     $cekuki = json_decode($cekuki[1]);
     if ($cekuki->status != "ok") {
-        $ulang = masuk($gip->username, $gip->password);
+        $ulang = masuk($username, $password);
         if ($ulang != "data berhasil diinput") {
             echo "Cookie Telah Mati, Gagal Membuat Ulang Cookie\n";
         } else {
@@ -38,26 +40,64 @@ $password = trim(fgets(STDIN));
         }
     } else {
         
-        $data = file_get_contents($gip->username.'.ig');
+        $data = file_get_contents($username.'.ig');
         $data = json_decode($data);
         
-        $mid = instagram(1, $data->useragent, 'feed/timeline/', $data->cookies);
-        $mid = json_decode($mid[1]);
-       for($a=1;$a<31104000;$a++):
-        foreach ($mid->items as $media) {
-            $like = instagram(1, $data->useragent, 'media/' . $media->pk . '/like/', $data->cookies, generateSignature('{"media_id":"' . $media->pk . '"}'));
-            $like = json_decode($like[1]);
-            if($like->status<>"ok"){
-            echo "Fail Like [" . $media->pk . "]\n";
-                }else{
-            echo "Success Like [" . $media->pk . "]\n";
+        $userid = instagram(1, $data->useragent, 'users/' . $username . '/usernameinfo', $data->cookies);
+        $userid = json_decode($userid[1]);
+        $userid = $userid->user->pk;
+        //print_r($userid);
+        
+        
+        $cekfoll = instagram(1, $data->useragent, 'friendships/' . $userid . '/following/', $data->cookies);
+        $cekfoll = json_decode($cekfoll[1]);
+        $cekfoll = array_slice($cekfoll->users, 0, $jumlah);
+        
+        foreach ($cekfoll as $ids) {
+            $cek = instagram(1, $data->useragent, 'friendships/show/' . $ids->pk, $data->cookies);
+            $cek = json_decode($cek[1]);
+
+            if(!file_exists('jeda-'.$username)){ 
+                $no = 1;
+            }else{
+                $no = file_get_contents('jeda-'.$username);
+            }
+            if($no%$jeda==0 AND $no>0):
+                echo "Jeda 180 detik.\n";
+                     $h=fopen("jeda-".$username,"w");
+                     fwrite($h,"0");
+                     fclose($h);
+                sleep(180);
+            endif;
+            if ($type == true) {
+                if ($cek->followed_by == false) {
+                    $unfollow = instagram(1, $data->useragent, 'friendships/destroy/' . $ids->pk . "/", $data->cookies, generateSignature('{"user_id":"' . $ids->pk . '"}'));
+                    $unfollow = json_decode($unfollow[1]);
+                    if($unfollow->status<>"ok"){
+                        echo "Fail Unfollow @" . $ids->username . " Because " . $unfollow->message . "\n";
+                        break;
+                    }else{
+                        echo "Success Unfollow @" . $ids->username . "\n";
+                        $h=fopen("jeda-".$username,"w");
+                        fwrite($h,$no+1);
+                        fclose($h);
+                        sleep(1);
+                    }
+                } else {
+                    echo "Fail Unfollow @" . $ids->username . " Users Follow You\n";
+                }
+            } else {
+                $unfollow = instagram(1, $data->useragent, 'friendships/destroy/' . $ids->pk . "/", $data->cookies, generateSignature('{"user_id":"' . $ids->pk . '"}'));
+                echo "Success Unfollow @" . $ids->username . "\n";
+                    $h=fopen("jeda-".$username,"w");
+                    fwrite($h,$no+1);
+                    fclose($h);
             }
         }
-        if($a%3==0){
-            echo "Menunggu $jeda Detik Untuk Sesi Berikutnya.\n";
-            sleep($jeda);
-        }
-       endfor;
+        
+        
     }
+    
 }
+
 ?>
